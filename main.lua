9ab0bb1863b89e29dae3ccc1eae1bccee0dd6ff2
Alex Bechanko <chanko08@gmail.com>
2014-11-16 21:05:42 -0500

    patched main with a cmd line arg for level select

--- main.lua
+++ main.lua
@@ -5,11 +5,16 @@ class   = require 'lib.hump.class'
 local State      = require('lib.hump.gamestate')
 local LevelState = require('states.level')
 
-function love.load()
+function love.load(args)
     local settings = require('settings')
     love.mouse.setVisible(false)
     love.window.setMode(settings.DISPLAY_WIDTH, settings.DISPLAY_HEIGHT, settings.WINDOW_OPTIONS)
 
     State.registerEvents()
-    State.switch(LevelState, settings.START_LEVEL)
+
+    local start_level = settings.START_LEVEL
+    if args[2] then
+        start_level = args[2]
+    end
+    State.switch(LevelState, start_level)
 end
