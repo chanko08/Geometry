953a14fa3596ea9be66c486f46abc6674a34ce37
Alex Bechanko <chanko08@gmail.com>
2015-04-12 16:32:03 -0400

    logging part 2

--- main.lua
+++ main.lua
@@ -1,12 +1,17 @@
 inspect = require('lib.inspect')
 class   = require 'lib.hump.class'
 _       = require 'lib.underscore'
+local Logger  = require 'utils.logger'
+
 
 local State      = require('lib.hump.gamestate')
 local LevelState = require('states.level')
 
 function love.load(args)
     local settings = require('settings')
+    print(inspect(args))
+    logger = Logger(_.slice(args, 2, #args))
+    logger:log('mom-joke', "yo momma is so fat, she needs a boomerang to put her belt on")
     love.mouse.setVisible(false)
     love.window.setMode(settings.DISPLAY_WIDTH, settings.DISPLAY_HEIGHT, settings.WINDOW_OPTIONS)
 
